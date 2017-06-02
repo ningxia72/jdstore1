2cc180d317524099c65348b280a4ce8981b000dd
夏宁 <ningxia72@gmail.com>
2017-06-02 10:40:48 +0800

    change uploader again

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,7 @@ else
   puts "Admin 已经建立过了，脚本跳过该步骤。"
 end
 
+# User.create!(name: 'admin', email: 'xia@test.com', password: '123456', password_confirmation: '123456', is_admin: true)
 
 # Initialize Product
 
@@ -26,26 +27,26 @@ Product.create!(title: "5斤装火龙果",
 description: "5斤装",
 price: 138,
 quantity: 5,
-image: open(src="http://oous4398f.bkt.clouddn.com/pitaya%20%E9%A2%98%E5%9B%BE.jpg")
+img_url: "https://cdn.shopify.com/s/files/1/0710/5755/files/Pitaya-Smooothie.jpg?14261920647843852612"
 )
 
 Product.create!(title: "9斤装火龙果",
 description: "9斤装",
 price: 198,
 quantity: 5,
-image: open(src="http://ww4.sinaimg.cn/large/006tNc79gy1fg4owd4ezvj31031xzkjm.jpg")
+img_url: "http://ww3.sinaimg.cn/large/006tKfTcgy1fg4vkl0p4cj30dw0dw75a.jpg"
 )
 
 Product.create!(title: "冰沙",
 description: "美味可口养颜",
 price: 68,
 quantity: 5,
-image: open("http://ww1.sinaimg.cn/large/006tNc79gy1fg3qjmmizgj30rb0hokfu.jpg)
-")
+img_url: "https://s-media-cache-ak0.pinimg.com/564x/be/29/47/be2947e7fc3dc4ae77cbc46e74351d6f.jpg"
 )
+
 Product.create!(title: "果园的清晨",
 description: "富含微量元素的土地",
 price: 88888,
 quantity: 0,
-image: open("http://ww4.sinaimg.cn/large/006tNc79gy1fg4owd4ezvj31031xzkjm.jpg")
+img_url: "http://ww3.sinaimg.cn/large/006tKfTcgy1fg4vkl0p4cj30dw0dw75a.jpg"
 )
